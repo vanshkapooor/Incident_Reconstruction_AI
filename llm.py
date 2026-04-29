e9a041e680373414175275137ab66ff335a5e290
Vansh Kapoor <vanshkapoor001@gmail.com>
2026-04-29 13:28:37 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,11 +1,26 @@
-import ollama
+import requests
+import os
 
-def call_llm(prompt, model="phi3"):
-    response = ollama.chat(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are an expert forensic investigator AI."},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return response['message']['content']
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+
+headers = {
+    "Authorization": f"Bearer {os.getenv('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')}"
+}
+
+def call_llm(prompt):
+    payload = {
+        "inputs": prompt,
+        "parameters": {
+            "max_new_tokens": 200,
+            "temperature": 0.3
+        }
+    }
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+
+    result = response.json()
+
+    if isinstance(result, list):
+        return result[0]["generated_text"]
+    else:
+        return str(result)
